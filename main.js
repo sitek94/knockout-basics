e8e37acb7458e0559e1522bdcb70374a018b43ef
sitek94 <msitkowski94@gmail.com>
2021-02-02 20:56:10 +0100

    make it possible to add/remove items

--- main.js
+++ main.js
@@ -1,7 +1,50 @@
-function ExampleViewModel() {
+var dummyData = [
+  { id: 'bone', icon: 'icon-bone', name: 'Bone', amount: 1 },
+  { id: 'ball', icon: 'icon-ball', name: 'Ball', amount: 2 },
+  { id: 'circle', icon: 'icon-circle', name: 'Circle', amount: 3 },
+  { id: 'rabbit', icon: 'icon-rabbit', name: 'Rabbit', amount: 4 },
+];
+
+function InventoryViewModel() {
   var self = this;
 
-  self.title = ko.observable('Hello World!');
+  self.name = ko.observable('');
+  self.icon = ko.observable('bone');
+  self.amount = ko.observable();
+
+  self.items = ko.observableArray(dummyData);
+
+  self.addItem = function (newItem) {
+    console.log(newItem);
+    if (!newItem) return;
+
+    self.items.push({
+      id: generateId(),
+      name: newItem.name,
+      icon: newItem.icon,
+      amount: newItem.amount,
+    });
+  };
+
+  self.removeItem = function (item) {
+    self.items.remove(item);
+  };
+
+  self.handleSubmit = function () {
+    self.addItem({
+      name: self.name(),
+      icon: self.icon(),
+      amount: self.amount(),
+    });
+  };
 }
 
-ko.applyBindings(new ExampleViewModel(), document.querySelector('#root'));
+ko.applyBindings(new InventoryViewModel(), document.querySelector('#root'));
+
+/**
+ * Very simple id generator, doesn't guarantee uniqueness, but it's
+ * absolutely fine for this simple example.
+ */
+function generateId() {
+  return 'id' + Math.random().toString(16).slice(2);
+}
